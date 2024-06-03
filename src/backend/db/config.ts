18f243b6acf48abf267bb435ac5817dcfbefe944
Zlaam <mrzalaam@gmail.com>
2024-06-04 00:09:30 +0500

    update 4 files and create 1 file

---
+++ src/backend/db/config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA8nxWQ6L-CU7QJmuKG_gyUP-8mfL23Vlc",
+  authDomain: "notes-zlaam.firebaseapp.com",
+  projectId: "notes-zlaam",
+  storageBucket: "notes-zlaam.appspot.com",
+  messagingSenderId: "140911491174",
+  appId: "1:140911491174:web:3f887c51747614968755a7",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,7 @@
 import {} from "react";
 
 function Home() {
+  console.log(import.meta.env.VITE_URI);
   return (
     <>
       <section>Home</section>
