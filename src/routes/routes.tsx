b19329c7a3167f8ff9ecd9f4c07f19e88516d782
Zlaam <mrzalaam@gmail.com>
2024-06-04 00:42:15 +0500

    add login form

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,8 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "@/pages/home/Home";
+import SignIn from "@/pages/auth/sign-in/SignIn";
 export const routes = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/auth/user/sign-in",
+    element: <SignIn />,
   },
 ]);
