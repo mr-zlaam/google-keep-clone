bac3d2bb5533d2e1334190ccb30eda50b7f048f9
Zlaam <mrzalaam@gmail.com>
2024-06-04 00:48:44 +0500

    update firebase.config.ts

--- src/backend/db/firebase.config.ts
+++ src/backend/db/firebase.config.ts
@@ -8,6 +8,9 @@ import {
   STORAGE_BUCKET,
 } from "@/config";
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -23,4 +26,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
+export const db = getFirestore(app);
