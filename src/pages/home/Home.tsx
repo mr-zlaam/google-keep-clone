fbc37eaae7e7f65ab0c1f463d678354014765928
Zlaam <mrzalaam@gmail.com>
2024-06-04 16:23:48 +0500

    add cards to home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,35 @@
-import { API_KEY } from "@/config";
+import { Card } from "@/components/ui/card";
 import {} from "react";
 
 function Home() {
-  console.log(API_KEY);
   return (
     <>
-      <section>Home</section>
+      <section className="grid justify-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        <Card className="h-[400px] bg-background shadow-lg cursor-pointer p-3 duration-300 transition-all hover:scale-105 m-3"></Card>
+        <Card className="h-[400px] bg-background shadow-lg cursor-pointer p-3 duration-300 transition-all hover:scale-105 m-3"></Card>
+        <Card className="h-[400px] bg-background shadow-lg cursor-pointer p-3 duration-300 transition-all hover:scale-105 m-3"></Card>
+        <Card className="h-[400px] bg-background shadow-lg cursor-pointer p-3 duration-300 transition-all hover:scale-105 m-3"></Card>
+        <Card className="h-[400px] bg-background shadow-lg cursor-pointer p-3 duration-300 transition-all hover:scale-105 m-3"></Card>
+        <Card className="h-[400px] bg-background shadow-lg cursor-pointer p-3 duration-300 transition-all hover:scale-105 m-3"></Card>
+        <Card className="h-[400px] bg-background shadow-lg cursor-pointer p-3 duration-300 transition-all hover:scale-105 m-3"></Card>
+        <Card className="h-[400px] bg-background shadow-lg cursor-pointer p-3 duration-300 transition-all hover:scale-105 m-3"></Card>
+        <Card className="h-[400px] bg-background shadow-lg cursor-pointer p-3 duration-300 transition-all hover:scale-105 m-3"></Card>
+        <Card className="h-[400px] bg-background shadow-lg cursor-pointer p-3 duration-300 transition-all hover:scale-105 m-3"></Card>
+        <Card className="h-[400px] bg-background shadow-lg cursor-pointer p-3 duration-300 transition-all hover:scale-105 m-3"></Card>
+        <Card className="h-[400px] bg-background shadow-lg cursor-pointer p-3 duration-300 transition-all hover:scale-105 m-3"></Card>
+        <Card className="h-[400px] bg-background shadow-lg cursor-pointer p-3 duration-300 transition-all hover:scale-105 m-3"></Card>
+        <Card className="h-[400px] bg-background shadow-lg cursor-pointer p-3 duration-300 transition-all hover:scale-105 m-3"></Card>
+        <Card className="h-[400px] bg-background shadow-lg cursor-pointer p-3 duration-300 transition-all hover:scale-105 m-3"></Card>
+        <Card className="h-[400px] bg-background shadow-lg cursor-pointer p-3 duration-300 transition-all hover:scale-105 m-3"></Card>
+        <Card className="h-[400px] bg-background shadow-lg cursor-pointer p-3 duration-300 transition-all hover:scale-105 m-3"></Card>
+        <Card className="h-[400px] bg-background shadow-lg cursor-pointer p-3 duration-300 transition-all hover:scale-105 m-3"></Card>
+        <Card className="h-[400px] bg-background shadow-lg cursor-pointer p-3 duration-300 transition-all hover:scale-105 m-3"></Card>
+        <Card className="h-[400px] bg-background shadow-lg cursor-pointer p-3 duration-300 transition-all hover:scale-105 m-3"></Card>
+        <Card className="h-[400px] bg-background shadow-lg cursor-pointer p-3 duration-300 transition-all hover:scale-105 m-3"></Card>
+        <Card className="h-[400px] bg-background shadow-lg cursor-pointer p-3 duration-300 transition-all hover:scale-105 m-3"></Card>
+        <Card className="h-[400px] bg-background shadow-lg cursor-pointer p-3 duration-300 transition-all hover:scale-105 m-3"></Card>
+        <Card className="h-[400px] bg-background shadow-lg cursor-pointer p-3 duration-300 transition-all hover:scale-105 m-3"></Card>
+      </section>
     </>
   );
 }
