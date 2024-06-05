90e4f945cd6a5c1267ac5bb93c5087fa6ad17601
Zlaam <mrzalaam@gmail.com>
2024-06-05 12:53:10 +0500

    update Profile.tsx

--- src/_components/profile/Profile.tsx
+++ src/_components/profile/Profile.tsx
@@ -3,7 +3,8 @@ import { useEffect, useState } from "react";
 import type { UserTypes } from "@/types";
 import { Card } from "@/components/ui/card";
 import { GetName } from "./components/GetName";
-import { X } from "lucide-react";
+import { LogOut, Plus, X } from "lucide-react";
+import { Button } from "@/components/ui/button";
 function Profile() {
   const [userDetails, setUserDetails] = useState<null | UserTypes>(null);
   const [isCardOpen, setIsCardOpen] = useState<boolean>(false);
@@ -43,7 +44,7 @@ function Profile() {
         </div>
       )}
       {isCardOpen && userDetails && (
-        <Card className="bg-background shadow-lg border  absolute top-2 right-4 h-[350px] w-[400px] flex flex-col">
+        <Card className="bg-blue-50 shadow-lg border  absolute top-2 right-4 h-[350px] w-[400px] flex flex-col">
           <button
             onClick={() => setIsCardOpen(false)}
             className="absolute rounded-full right-2 top-2 w-[35px] h-[35px] flex justify-center items-center cursor-pointer duration-300 transition-all hover:bg-foreground/10 "
@@ -64,6 +65,20 @@ function Profile() {
               "John Doe".split(" ")[0]}
             !
           </h1>
+          <div className="flex items-center mx-auto my-4 overflow-hidden border rounded-full border-foreground/10 w-fit">
+            <Button
+              variant={"outline"}
+              className="flex items-center px-4 transition-all duration-200 bg-transparent border-r border-none rounded py-7 hover:bg-background "
+            >
+              <Plus className="mx-2" /> Add Another Account
+            </Button>
+            <Button
+              variant={"outline"}
+              className="flex items-center px-4 transition-all duration-200 bg-transparent border-r border-none rounded py-7 hover:bg-background"
+            >
+              <LogOut className="mx-2" /> Sign out
+            </Button>
+          </div>
         </Card>
       )}
     </>
