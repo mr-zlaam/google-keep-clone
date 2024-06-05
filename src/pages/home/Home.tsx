51045e93dbbf9cf326800629c87bbb5f552e528b
Zlaam <mrzalaam@gmail.com>
2024-06-05 15:08:02 +0500

    update Home.tsx

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,7 +4,7 @@ import {} from "react";
 function Home() {
   return (
     <>
-      <section className="grid justify-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      <section className="grid justify-center grid-cols-1 px-5 md:grid-cols-2 lg:grid-cols-3">
         <Card className="h-[400px] bg-background shadow-lg cursor-pointer p-3 duration-300 transition-all hover:scale-105 m-3"></Card>
         <Card className="h-[400px] bg-background shadow-lg cursor-pointer p-3 duration-300 transition-all hover:scale-105 m-3"></Card>
         <Card className="h-[400px] bg-background shadow-lg cursor-pointer p-3 duration-300 transition-all hover:scale-105 m-3"></Card>
