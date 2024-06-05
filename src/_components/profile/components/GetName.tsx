63ac3ab88cb50bb0028f22bd18dc0115ea90a524
Zlaam <mrzalaam@gmail.com>
2024-06-05 12:36:54 +0500

    update GetName.tsx

--- src/_components/profile/components/GetName.tsx
+++ src/_components/profile/components/GetName.tsx
@@ -1,5 +1,4 @@
 export const GetName = ({ name }: { name: string }) => {
-  // Function to get the initials
   const getInitials = (name: string) => {
     const words = name.split(" ");
     const initials = words.map((word: string) => word.charAt(0).toUpperCase());
