18e5f7ea1ff4c0a0b4973a754600c1bc8ed4c80d
Zlaam <mrzalaam@gmail.com>
2024-06-05 10:16:45 +0500

    update 1 file and copy 1 file

--- src/Layouts/RootLayout.tsx
+++ src/Layouts/RootLayout.tsx
@@ -1,24 +1,8 @@
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { useEffect, useState } from "react";
-import { Outlet, useNavigate } from "react-router-dom";
-import Cookies from "universal-cookie";
+import { RouteProtector } from "@/protectedRoute";
+import { Outlet } from "react-router-dom";
 
 function RootLayout() {
-  const cookie = new Cookies();
-  const navigate = useNavigate();
-  const [isLogin, setIsLogin] = useState(false);
-  const auth = getAuth();
-  const uidFromCookie = cookie.get("uid");
-  console.log(uidFromCookie);
-  useEffect(() => {
-    if (!uidFromCookie) return navigate("/auth/user/sign-up");
-    onAuthStateChanged(auth, (user) => {
-      console.log("isEqual", user?.uid === uidFromCookie);
-      if (user?.uid === uidFromCookie) setIsLogin(true);
-      else return navigate("/auth/user/sign-up");
-    });
-  }, []);
-
+  const isLogin = RouteProtector();
   return (
     <>
       <main>{isLogin && <Outlet />}</main>
---
+++ src/protectedRoute/index.ts
@@ -0,0 +1,20 @@
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Cookies from "universal-cookie";
+
+export const RouteProtector = () => {
+  const cookie = new Cookies();
+  const navigate = useNavigate();
+  const [isLogin, setIsLogin] = useState(false);
+  const auth = getAuth();
+  const uidFromCookie = cookie.get("uid");
+  useEffect(() => {
+    if (!uidFromCookie) return navigate("/auth/user/sign-up");
+    onAuthStateChanged(auth, (user) => {
+      if (user?.uid === uidFromCookie) setIsLogin(true);
+      else return navigate("/auth/user/sign-up");
+    });
+  }, []);
+  return isLogin;
+};
