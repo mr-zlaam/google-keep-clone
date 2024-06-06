7d2b2a9964bf4fdd0726ced712ee3b581c4b7875
Zlaam <mrzalaam@gmail.com>
2024-06-06 12:45:38 +0500

    update Profile.tsx

--- src/_components/profile/Profile.tsx
+++ src/_components/profile/Profile.tsx
@@ -9,16 +9,22 @@ import { Link, useNavigate } from "react-router-dom";
 import { auth as Auth } from "@/backend/db/firebase.config";
 import Cookies from "universal-cookie";
 import { useMessage } from "@/hooks/useMessage";
+import useLoading from "@/hooks/useLoading";
+import Loader from "../loding/Loader";
 function Profile() {
   const cookie = new Cookies();
   const navigate = useNavigate();
   const { successMessage, errorMessage } = useMessage();
   const [userDetails, setUserDetails] = useState<null | UserTypes>(null);
   const [isCardOpen, setIsCardOpen] = useState<boolean>(false);
+  const { isLoading, startLoading, stopLoading } = useLoading();
   const auth = getAuth();
   useEffect(() => {
+    startLoading();
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) setUserDetails(user);
+      stopLoading();
+      return;
     });
     return () => unsubscribe();
   }, []);
@@ -63,55 +69,59 @@ function Profile() {
           )}
         </div>
       )}
-      {isCardOpen && userDetails && (
-        <Card className=" shadow-lg border p-4 absolute top-2 right-4 h-[350px] w-[400px] flex flex-col">
-          <button
-            onClick={() => setIsCardOpen(false)}
-            className="absolute rounded-full right-2 top-2 w-[35px] h-[35px] flex justify-center items-center cursor-pointer duration-300 transition-all hover:bg-foreground/10 "
-          >
-            <X />
-          </button>
-          <h1 className="my-2 text-center">{userDetails.email}</h1>
-          <div className="mx-auto my-3 cursor-pointer flex bg-green-700 select-none items-center justify-center border overflow-hidden rounded-full h-[70px] w-[70px] border-foreground/30">
-            <img
-              src={userDetails?.photoURL || ""}
-              alt={userDetails.displayName as string}
-              className="object-contain w-auto h-auto mx-auto rounded-full"
-            />
-          </div>
-          <h1 className="text-2xl font-medium text-center">
-            Hi,{" "}
-            {userDetails?.displayName?.split(" ")[0] ||
-              "John Doe".split(" ")[0]}
-            !
-          </h1>
-          <Link
-            className="flex items-center justify-center py-2 mx-auto my-4 text-sm text-blue-500 transition-all duration-300 border rounded-full hover:bg-background/80 px-7 w-fit border-foreground/60"
-            to={"https://myaccount.google.com/"}
-            target="_blank"
-          >
-            <span>Manage Your Google Account</span>
-          </Link>
-          <div className="flex items-center mx-auto my-4 overflow-hidden border rounded-full border-foreground/10 w-fit">
-            <Link to={"https://accounts.google.com/"} target="_blank">
-              <Button
-                variant={"outline"}
-                className="flex items-center px-4 transition-all duration-200 border-none rounded-none bg-background py-7 hover:bg-foreground/5 "
-              >
-                <Plus className="mx-2" /> Add Another Account
-              </Button>
-            </Link>
-            <div className="w-[1px] bg-foreground/10 h-full" />
-            <Button
-              onClick={handleLogout}
-              variant={"outline"}
-              className="flex items-center px-4 transition-all duration-200 border-none rounded-none bg-background py-7 hover:bg-foreground/5"
+      {isCardOpen &&
+        userDetails &&
+        (isLoading ? (
+          <Loader />
+        ) : (
+          <Card className=" shadow-lg border p-4 absolute top-2 right-4 h-[350px] w-[400px] flex flex-col">
+            <button
+              onClick={() => setIsCardOpen(false)}
+              className="absolute rounded-full right-2 top-2 w-[35px] h-[35px] flex justify-center items-center cursor-pointer duration-300 transition-all hover:bg-foreground/10 "
             >
-              <LogOut className="mx-2" /> Sign out
-            </Button>
-          </div>
-        </Card>
-      )}
+              <X />
+            </button>
+            <h1 className="my-2 text-center">{userDetails.email}</h1>
+            <div className="mx-auto my-3 cursor-pointer flex bg-green-700 select-none items-center justify-center border overflow-hidden rounded-full h-[70px] w-[70px] border-foreground/30">
+              <img
+                src={userDetails?.photoURL || ""}
+                alt={userDetails.displayName as string}
+                className="object-contain w-auto h-auto mx-auto rounded-full"
+              />
+            </div>
+            <h1 className="text-2xl font-medium text-center">
+              Hi,{" "}
+              {userDetails?.displayName?.split(" ")[0] ||
+                "John Doe".split(" ")[0]}
+              !
+            </h1>
+            <Link
+              className="flex items-center justify-center py-2 mx-auto my-4 text-sm text-blue-500 transition-all duration-300 border rounded-full hover:bg-background/80 px-7 w-fit border-foreground/60"
+              to={"https://myaccount.google.com/"}
+              target="_blank"
+            >
+              <span>Manage Your Google Account</span>
+            </Link>
+            <div className="flex items-center mx-auto my-4 overflow-hidden border rounded-full border-foreground/10 w-fit">
+              <Link to={"https://accounts.google.com/"} target="_blank">
+                <Button
+                  variant={"outline"}
+                  className="flex items-center px-4 transition-all duration-200 border-none rounded-none bg-background py-7 hover:bg-foreground/5 "
+                >
+                  <Plus className="mx-2" /> Add Another Account
+                </Button>
+              </Link>
+              <div className="w-[1px] bg-foreground/10 h-full" />
+              <Button
+                onClick={handleLogout}
+                variant={"outline"}
+                className="flex items-center px-4 transition-all duration-200 border-none rounded-none bg-background py-7 hover:bg-foreground/5"
+              >
+                <LogOut className="mx-2" /> Sign out
+              </Button>
+            </div>
+          </Card>
+        ))}
     </>
   );
 }
