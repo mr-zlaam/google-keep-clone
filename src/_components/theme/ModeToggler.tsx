6c5654bce030d1701ccbcae6f990d79dbd3525eb
Zlaam <mrzalaam@gmail.com>
2024-06-06 11:12:50 +0500

    update ModeToggler.tsx, theme-provider.tsx and Home.tsx

--- src/_components/theme/ModeToggler.tsx
+++ src/_components/theme/ModeToggler.tsx
@@ -1,39 +1,30 @@
-import { Moon, Sun } from "lucide-react";
+import { MoonIcon, SunIcon } from "lucide-react";
 
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import { useTheme } from "@/_components/theme/theme-provider";
 
 export function ModeToggler() {
-  const { setTheme } = useTheme();
-
+  const { setTheme, theme } = useTheme();
   return (
-    <div className="mx-4">
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="ghost" size="icon">
-            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-            <span className="sr-only">Toggle theme</span>
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-          <DropdownMenuItem onClick={() => setTheme("light")}>
-            Light
-          </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setTheme("dark")}>
-            Dark
-          </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setTheme("system")}>
-            System
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </div>
+    <>
+      {theme && theme === "dark" ? (
+        <div
+          onClick={() => {
+            setTheme("light");
+          }}
+          className="mx-4 h-[45px] w-[45px] rounded-full cursor-pointer flex items-center justify-center duration-300 transition-all hover:bg-foreground/15 "
+        >
+          <SunIcon />
+        </div>
+      ) : (
+        <div
+          onClick={() => {
+            setTheme("dark");
+          }}
+          className="mx-4 h-[45px] w-[45px] rounded-full cursor-pointer flex items-center justify-center duration-300 transition-all hover:bg-foreground/15 "
+        >
+          <MoonIcon />
+        </div>
+      )}
+    </>
   );
 }
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -5,9 +5,9 @@ function Home() {
   return (
     <>
       <section className="grid justify-center grid-cols-1 px-5 md:grid-cols-2 lg:grid-cols-3">
-        <Card className="h-[400px] bg-background shadow-lg cursor-pointer p-3 duration-300 transition-all hover:scale-105 m-3"></Card>
-        <Card className="h-[400px] bg-background shadow-lg cursor-pointer p-3 duration-300 transition-all hover:scale-105 m-3"></Card>
-        <Card className="h-[400px] bg-background shadow-lg cursor-pointer p-3 duration-300 transition-all hover:scale-105 m-3"></Card>
+        <Card className="h-[400px] bg-background shadow-lg cursor-pointer p-3 m-3"></Card>
+        <Card className="h-[400px] bg-background shadow-lg cursor-pointer p-3 m-3"></Card>
+        <Card className="h-[400px] bg-background shadow-lg cursor-pointer p-3 m-3"></Card>
       </section>
     </>
   );
