ccac428898e627b7b1e903e5cc45d0163f85a796
Zlaam <mrzalaam@gmail.com>
2024-06-06 13:04:04 +0500

    update Search.tsx

--- src/_components/Search/Search.tsx
+++ src/_components/Search/Search.tsx
@@ -1,13 +1,20 @@
 import { Input } from "@/components/ui/input";
-import {} from "react";
-import { Search as SearchIcon, X } from "lucide-react";
+import { useState } from "react";
+import { Search as SearchIcon, X as DeleteIcon } from "lucide-react";
 import DivWrapper from "../DivWrapper/DivWrapper";
 
 function Search() {
+  const [searchData, setSearchData] = useState("");
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newData = e.target.value;
+    setSearchData(newData);
+  };
   return (
     <>
       <div className="mx-4 bg-background   search md:w-[400px]  rounded-md h-12 relative overflow-hidden">
         <Input
+          value={searchData}
+          onChange={handleOnChange}
           placeholder="Search"
           className="h-full px-12 text-lg font-medium border border-foreground/60 focus:border-2"
         />
@@ -15,9 +22,16 @@ function Search() {
           className="absolute top-3.5 left-2 text-foreground"
           size={20}
         />
-        <DivWrapper className="absolute top-2.5 right-0 cursor-pointer text-foreground h-[30px] w-[30px]">
-          <X className="" size={20} />
-        </DivWrapper>
+        {searchData.length > 0 && (
+          <DivWrapper
+            onClick={() => {
+              setSearchData("");
+            }}
+            className="absolute top-2.5 right-0 cursor-pointer text-foreground h-[30px] w-[30px]"
+          >
+            <DeleteIcon className="" size={20} />
+          </DivWrapper>
+        )}
         {/*show only when something is wrriten*/}
       </div>
     </>
