a03460c95b47a577c17b2b049626072739875f5f
Zlaam <mrzalaam@gmail.com>
2024-06-06 10:29:48 +0500

    add userStatus

--- src/Layouts/RootLayout.tsx
+++ src/Layouts/RootLayout.tsx
@@ -1,15 +1,25 @@
 import Navbar from "@/_components/navbar/Navbar";
+import useOnlineStatus from "@/hooks/useStatus";
 import { useLoginChecker } from "@/protectedRoute";
 import { Outlet } from "react-router-dom";
 
 function RootLayout() {
   const isLogin = useLoginChecker();
+  const isOnline = useOnlineStatus();
   return (
     <>
-      <main className="">
-        <Navbar />
-        {isLogin && <Outlet />}
-      </main>
+      {isOnline ? (
+        <main className="">
+          <Navbar />
+          {isLogin && <Outlet />}
+        </main>
+      ) : (
+        <div className="flex items-center justify-center h-screen bg-foreground">
+          <h1 className="text-4xl font-bold text-red-500">
+            You are offline please connect to the internet!!
+          </h1>
+        </div>
+      )}
     </>
   );
 }
