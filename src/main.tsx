bd3cb35706d47b0a71503af748691671cd120b30
Zlaam <mrzalaam@gmail.com>
2024-06-06 11:12:44 +0500

    add theme toggling

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { routes } from "./routes/routes.tsx";
 import { ThemeProvider } from "./_components/theme/theme-provider.tsx";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme="light" storageKey="theme">
       <RouterProvider router={routes} />
     </ThemeProvider>
   </React.StrictMode>
