75cf258de42c7f708013e2ce7862ed4b3e6a6d2c
Zlaam <mrzalaam@gmail.com>
2024-06-06 10:40:10 +0500

    update 4 files and create 3 files

--- src/_components/navbar/Navbar.tsx
+++ src/_components/navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import {} from "react";
 import Search from "../Search/Search";
 import Profile from "../profile/Profile";
+import { ModeToggler } from "../theme/ModeToggler";
 
 function Navbar() {
   return (
@@ -8,7 +9,10 @@ function Navbar() {
       <header className="h-[80px] w-full bg-background shadow-md text-foreground flex justify-around items-center ">
         <h1>Logo</h1>
         <Search />
-        <Profile />
+        <div className="flex items-center justify-center gap-3">
+          <ModeToggler />
+          <Profile />
+        </div>
       </header>
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,11 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import { routes } from "./routes/routes.tsx";
+import { ThemeProvider } from "./_components/theme/theme-provider.tsx";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={routes} />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <RouterProvider router={routes} />
+    </ThemeProvider>
   </React.StrictMode>
 );
