401508fad7b4619d27ad6fa0e40eae9198d420d2
Zlaam <mrzalaam@gmail.com>
2024-06-10 14:32:01 +0500

    update index.html and CreateNote.tsx

--- src/_components/createNote/CreateNote.tsx
+++ src/_components/createNote/CreateNote.tsx
@@ -131,12 +131,14 @@ function CreateNote({ setIsUploaded }: CreateNoteProp) {
                   >
                     save
                   </Button>
-                  <Button
-                    className="absolute py-4 bottom-2 left-4"
-                    onClick={() => setIsNoteOpen((prev) => !prev)}
-                  >
-                    cancel
-                  </Button>
+                  {data.title && data.description && (
+                    <Button
+                      className="absolute py-4 bottom-2 left-4"
+                      onClick={() => setIsNoteOpen((prev) => !prev)}
+                    >
+                      cancel
+                    </Button>
+                  )}
                 </div>
               </form>
             </section>
